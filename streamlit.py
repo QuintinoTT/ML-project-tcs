e28b53891ef27f81933e822ed5a8994ca6de6c84
ThePlazma-Tic <wowchik.unreal@gmail.com>
2025-10-20 17:06:55 +0200

    Update streamlit.py
    a

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
-import thatonefile as *
+import thatonefile as tof
 
 
 st.write("Melbourne housing")
 if st.button("test"):
-    st.dataframe(datasetcall())
+    st.dataframe(tof.datasetcall())
 
 st.area_chart(ds.dataset ,x="testx", y="testy")
+
