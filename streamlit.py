0759489504e8a2a09b4a822682ebb225aa032b35
ThePlazma-Tic <wowchik.unreal@gmail.com>
2025-10-21 11:08:26 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,10 +10,18 @@ with st.container(border=True, gap=None):
 c1 = st.container(border=True)
 c1.write(data.dataset)
 col1, col2 = st.columns(2)
-col1.chat_input(placeholder="testinput")
-col1.chat_input(placeholder="testinput2")
+budgetinp=col1.chat_input(placeholder="testinput")
+budgetreginp=col1.chat_input(placeholder="testinput2")
 col1.chat_input(placeholder="testinput3.1")
 col2.chat_input(placeholder="testinput3")
+if st.button("Budget stuff"):
+  regionB = budgetreginp
+  pricevarB = budgetinp
+  write(query("select *, "+pricevarB+"-price as Budget_deviation, price FROM dataset  WHERE CouncilArea = \""+regionB+"\" order by ABS(Budget_deviation) LIMIT 15 "))
+  
+
+
+
 
 
 
