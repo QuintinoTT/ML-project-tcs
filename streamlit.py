47a8c524c711375b4d4fc6471e80399c417dc470
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 10:24:11 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,9 +7,11 @@ import thatonefile as data
 
 
 st.write("Melbourne housing")
+st.write(dataset)
 if st.button("test"):
   st.write("test complete")
 #st.area_chart(tof.dataset, x="testx", y="testy")
+
 
 
 
