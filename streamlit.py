316bb805787e70e5fd60d5a402d073a679f4035c
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 10:55:51 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,11 +6,12 @@ import dataprocessing as data
 
 
 st.header("Melbourne housing")
-c1 = st.container(border=True, height=300)
+c1 = st.container(border=True)
 c1.write(data.dataset)
 col1, col2 = st.columns(2)
-col1.write("test")
+col1.chat_input(placeholder="testinput")
 col2.write("test2")
+
 
 
 
