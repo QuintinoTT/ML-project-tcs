ff523cf75a32cff55cda061506354024121ea5cb
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 09:29:27 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,13 @@
 import streamlit as st
+import pandas as pd
+import sklean as sl
 
 st.write("Melbourne housing")
 if st.button("test"):
    #st.dataframe(thatonefille.datasetcall())
 
    st.area_chart(tof.dataset ,x="testx", y="testy")
+
 
 
 
