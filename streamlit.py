f30efca92133544b9e70f7689c300d049f961392
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 10:44:45 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,6 +5,7 @@ import pandasql as ps
 import dataprocessing as data
 
 
-st.header("Melbourne housing")
+st.header("Melbourne housing", horizontal_alignment="left", vertical_alignment="top")
 st.container(data.dataset)
+
 
