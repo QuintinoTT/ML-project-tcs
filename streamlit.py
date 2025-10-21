40e5c30be3d1b346f2b33a57ea99ab7d766f4847
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 15:44:14 +0200

    test

--- streamlit.py
+++ streamlit.py
@@ -9,6 +9,7 @@ dataset = data.dataset
 with st.container(border=True):
   query_col, push_query_col = st.columns([3,1])
   sqlquery = query_col.text_input(label="", placeholder="write your SQL query here")
+  push_query_col.write("test")
   if push_query_col.button("push query"):
     st.write(data.query(sqlquery))
 
