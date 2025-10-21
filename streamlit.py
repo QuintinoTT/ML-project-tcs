0003ce2bfcc8d90a1f05615ae4fc84cad7c4c0ee
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 11:18:20 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -13,11 +13,12 @@ col1, col2 = st.columns(2)
 budgetinp=col1.chat_input(placeholder="Budget")
 budgetreginp=col1.chat_input(placeholder="Reg")
 if st.button("Budget stuff"):
-  regionB = str(budgetreginp)
-  pricevarB = str(budgetinp)
+  regionB = str({budgetreginp})
+  pricevarB = str({budgetinp})
   st.write(regionB, pricevarB)
   st.write(data.query("select *, "+pricevarB+"-price as Budget_deviation, price FROM dataset  WHERE CouncilArea = \""+regionB+"\" order by ABS(Budget_deviation) LIMIT 15 "))
   
+
 
 
 
