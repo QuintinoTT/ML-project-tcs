59ad67b6a61d557a1f68d3d0e23529cc6b0721a1
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 09:59:04 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 import sklearn as sl
--m pip show pandasql  
+st.write(-m pip show pandasql)
 
 st.write("Melbourne housing")
 if st.button("test"):
   st.write("test complete")
 #st.area_chart(tof.dataset, x="testx", y="testy")
+
 
 
 
