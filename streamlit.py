c58680c553553eef567b8693704a9f9d262de6d0
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 15:27:10 +0200

    clean up + comments

--- streamlit.py
+++ streamlit.py
@@ -14,6 +14,8 @@ with st.container(border=True, gap=None): #top header
 with st.container(border=True): #fist query; price prediction
   st.subheader("Price prediction:")
   feature_col, data_col = st.columns([1,3])
+
+  #drop down boxes for price prediction
   select_type = feature_col.selectbox(
     "Type:", 
     data.query(
@@ -32,6 +34,8 @@ with st.container(border=True): #fist query; price prediction
       "SELECT distinct Method FROM dataset"), 
     placeholder="Select method",
   )
+
+  #text inputs for price prediction
   select_rooms = feature_col.text_input(label="Amount of rooms:")
   select_distance = feature_col.text_input(label="Distance to centre:")
   select_bedroom = feature_col.text_input(label="Amount of bedrooms:")
@@ -43,9 +47,8 @@ with st.container(border=True): #fist query; price prediction
 
 
 with st.container(border=True): #second query
-  col1, col2 = st.columns(2, width="stretch")
-  col1.write(dataset) #display dataset
-  budgetinp=st.text_input(label="Budget:",placeholder="Select budget")
+  st.write(dataset) #display dataset
+  budgetinp=st.text_input(label="Budget:", placeholder="Select budget")
   budgetreginp=st.selectbox(
     "Region:", 
     data.query(
@@ -55,7 +58,7 @@ with st.container(border=True): #second query
   if st.button("Budget stuff"):
     regionB = budgetreginp
     pricevarB = str(budgetinp)
-    col2.write(data.query(
+    st.write(data.query(
       "SELECT *, "+pricevarB+"-price AS Budget_deviation FROM dataset  WHERE CouncilArea = \""+regionB+"\" ORDER BY ABS(Budget_deviation) LIMIT 15 ")) #change query to select less stuff and sort by price deviation
 
 
