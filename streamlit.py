7b223edb8d5f559413e1171125b9ee9668fd2c1c
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 11:51:08 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,25 +10,24 @@ with st.container(border=True, gap=None): #top header
   st.header("Melbourne housing", divider="blue")
   st.write("Predicting housing prices using kNN Machine Learning")
 
-
-c1 = st.container(border=True)
-c1.write(dataset) #display dataset
-
-col1, col2 = st.columns(2) #textinput + query output
-budgetinp=col1.text_input(label="Budget",placeholder="Budget")
-budgetreginp=col1.selectbox(
-  "Region:", 
-  data.query(
-    "SELECT distinct CouncilArea FROM dataset"), 
-  placeholder="Select region",
-)
-with c1:
+with st.container(border=True)
+  col1, col2 = columns(2)
+    col1.write(dataset) #display dataset
   if st.button("Budget stuff"):
     regionB = budgetreginp
     pricevarB = str(budgetinp)
     st.write(regionB, pricevarB)
-    st.write(data.query(
+    col2.write(data.query(
       "SELECT *, "+pricevarB+"-price AS Budget_deviation FROM dataset  WHERE CouncilArea = \""+regionB+"\" ORDER BY ABS(Budget_deviation) LIMIT 15 "))
+    budgetinp=st.text_input(label="Budget",placeholder="Budget")
+  budgetreginp=st.selectbox(
+    "Region:", 
+    data.query(
+      "SELECT distinct CouncilArea FROM dataset"), 
+    placeholder="Select region",
+  )
+
+
 
 
 
