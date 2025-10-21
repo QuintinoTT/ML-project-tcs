e99570ad735b0fb05e0476045007659c34dde8cc
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 10:57:20 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -11,7 +11,9 @@ c1.write(data.dataset)
 col1, col2 = st.columns(2)
 col1.chat_input(placeholder="testinput")
 col1.chat_input(placeholder="testinput2")
+col1.chat_input(placeholder="testinput3.1")
 col2.chat_input(placeholder="testinput3")
+
 
 
 
