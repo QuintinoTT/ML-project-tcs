452dc163ed051fb0686931e003e43073043f339a
QuintinoTT <qstoelinga@gmail.com>
2025-10-21 09:21:32 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,13 +1,11 @@
 import streamlit as st
-import pandas as pd
-import thatonefile
-
 
 st.write("Melbourne housing")
 if st.button("test"):
    #st.dataframe(thatonefille.datasetcall())
 
    st.area_chart(tof.dataset ,x="testx", y="testy")
+
 
 
 
