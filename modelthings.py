dba9852d78c2d30074113b7263196de14a6a776d
ThePlazma-Tic <wowchik.unreal@gmail.com>
2025-10-28 00:01:44 +0100

    Update modelthings.py

--- modelthings.py
+++ modelthings.py
@@ -9,6 +9,5 @@ def Dealwithinputdata(dataframe):
   dummiedframe=pd.get_dummies(dataframe)
   dummiedframe.astype(int)
   ghostrowc = ghostrow.copy()
-  ghostrowc=ghostrowc.drop(columns=["Unnamed:0"])
-
+ 
   return ghostrowc 
