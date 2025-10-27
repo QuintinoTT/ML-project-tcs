f42e2caef0a6d8d0ab82049c8ff1a816403344f5
ThePlazma-Tic <wowchik.unreal@gmail.com>
2025-10-27 15:45:42 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -59,12 +59,6 @@ with st.container(border=True): #fist query; price prediction
 with st.container(border=True): #second query
   st.header("Feature importance estimation")
   inputcolumn, outputcolumn = st.columns([1,3])
-  councilareachoice = inputcolumn.selectbox(
-    "Region:", 
-    data.query(
-      "SELECT distinct CouncilArea FROM dataset"), 
-    placeholder="Council area",
-  )
 
 
 
@@ -86,6 +80,7 @@ with st.container(border=True): #third query
     pricevarB = str(budgetinp)
     st.write(data.query(
       "SELECT *, "+pricevarB+"-price AS Budget_deviation FROM dataset  WHERE CouncilArea = \""+regionB+"\" ORDER BY ABS(Budget_deviation) LIMIT 15 ")) #change query to select less stuff and sort by price deviation
+
 
 
 
