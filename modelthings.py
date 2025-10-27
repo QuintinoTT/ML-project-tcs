e7ebe84e6ca19a196d874a936308ea616ea91b33
ThePlazma-Tic <wowchik.unreal@gmail.com>
2025-10-27 16:07:44 +0100

    Update modelthings.py

--- modelthings.py
+++ modelthings.py
@@ -1,3 +1,6 @@
 from joblib import load
 model = load("knn_model_price_prediction.joblib")
 scaler = load("scaler_price_prediction.joblib")
+def dataframescale(dataframe):
+  scaledframe=scaler.transform(dataframe)
+  return scaledframe
