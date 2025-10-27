3872b00e3925a8cfc70efa4f46f77a8ca4a0b4e2
ThePlazma-Tic <wowchik.unreal@gmail.com>
2025-10-27 23:23:21 +0100

    Update modelthings.py

--- modelthings.py
+++ modelthings.py
@@ -6,6 +6,7 @@ scaler = load("scale.joblib")
 def Dealwithinputdata(dataframe):
   dummiedframe=pd.get_dummies(dataframe)
   ghostrowc = ghostrow.copy()
+  print(ghostrowc)
   for columns in ghostrowc:
         if columns in dummiedframe.columns:
            ghostrowc.at[0, columns] = dummiedframe.at[0, columns]
