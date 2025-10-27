cfc8ee88f70bd3dc8bc86461e2bdac3d0c549df3
ThePlazma-Tic <wowchik.unreal@gmail.com>
2025-10-27 23:53:52 +0100

    Update modelthings.py

--- modelthings.py
+++ modelthings.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from joblib import load
-ghostrow = pd.read_csv('ghostframe.csv', index=False)
+ghostrow = pd.read_csv('ghostframe.csv')
 model = load("mdl.joblib")
 scaler = load("scale.joblib")
 def Dealwithinputdata(dataframe):
