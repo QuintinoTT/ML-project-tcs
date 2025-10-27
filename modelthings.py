6ac4dadad3628a7fbed2262d0d3a0b2319213c69
ThePlazma-Tic <wowchik.unreal@gmail.com>
2025-10-27 20:32:57 +0100

    Update modelthings.py

--- modelthings.py
+++ modelthings.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from joblib import load
+ghostrow = pd.read_csv(ghostframe.csv)
 model = load("knn_model_price_prediction.joblib")
 scaler = load("scaler_price_prediction.joblib")
 def Dealwithinputdata(dataframe):
   dummiedframe=pd.get_dummies(dataframe)
-  
-  scaledframe=scaler.transform(dataframe)
+  ghostrowc = ghostrow.copy()
+  for columns in ghostrowc:
+        if columns in dummiedframe.columns:
+           ghostrowc.at[0, columns] = dummiedframe.at[0, columns]
+  scaledframe=scaler.transform(ghostrowc)
   return scaledframe 
