8388227aa12d6cb93f90b14cea0a337d7899c950
ThePlazma-Tic <wowchik.unreal@gmail.com>
2025-10-28 01:23:11 +0100

    Update modelthings.py

--- modelthings.py
+++ modelthings.py
@@ -1,18 +1,3 @@
 import streamlit as st
 import pandas as pd
-from joblib import load
-ghostrow = pd.read_csv('ghostframe.csv')
-
-model = load("model.joblib")
-scaler = load("scalr.joblib")
-def Dealwithinputdata(dataframe):
-  dummiedframe=pd.get_dummies(dataframe)
-  dummiedframe.astype(int)
-  ghostrowc = ghostrow.copy()
-  ghostrowc=ghostrowc.drop(columns=["Unnamed: 0"])
-  for columns in ghostrowc:
-      if columns in dummiedframe.columns:
-         ghostrowc.at[0, columns] = dummiedframe.at[0, columns]
-  scaledframe=scaler.transform(ghostrowc)
-  return scaledframe
 
